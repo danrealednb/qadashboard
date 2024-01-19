b8062d0b5266d5c27adc1d0c717a8e00f0150379
Dan Reale <realed@dnb.com>
2024-01-19 18:25:17 -0500

    remove some commented code

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,29 +1,18 @@
-import { json, type MetaFunction } from "@remix-run/node";
+import { type MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import CountVisualWithLink from "~/components/ChartVisualWithLink";
-import CountPercentageVisual from "~/components/CountPercentageVisual";
-import CountVisual from "~/components/CountVisual";
+
 import Header from "~/components/Header";
 import PercentageVisual from "~/components/PercentageVisual";
 
 import StarbasePieChart from "~/components/StarbasePieChart";
-import BasicTooltip from "~/components/Tooltip";
+
 import {
   getJiraBugs30Days,
   getJiraBugs30DaysDev,
   getJiraBugs30DaysProd,
-  getJiraData,
 } from "~/data/jira.server";
-import {
-  getAllTestCases,
-  getAutomatedTests,
-  getCurrentTestRuns,
-  getManualTests,
-  getPercentage,
-  getTestCasesFromTestRail,
-  TEST_RUN_DATA,
-  testRunChartData,
-} from "~/data/testrail.server";
+import { getCurrentTestRuns, TEST_RUN_DATA } from "~/data/testrail.server";
 
 export const meta: MetaFunction = () => {
   return [
@@ -94,52 +83,12 @@ export default function Index() {
           page="/defects30dev"
         />
       </div>
-      {/* <div>
-        <ul className="grid justify-center space-y-2">
-          {testRailData.map((testEntry: any, index: number) => (
-            <li className="grid justify-center" key={index}>
-              <label htmlFor="">Test Id {testEntry.test_id}</label>
-              <label htmlFor="">Status {testEntry.status_id}</label>
-              <label htmlFor="">Created On {testEntry.created_on}</label>
-              <label htmlFor="">Environment {testEntry.version}</label>
-              <label htmlFor="">Duration {testEntry.elapsed}</label>
-            </li>
-          ))}
-        </ul>
-      </div> */}
     </>
   );
 }
 
 export async function loader() {
-  // const headers = {
-  //   Authorization: `Basic ${process.env.TEST_RAIL_API_KEY}`,
-  // };
-  // const res = await fetch(
-  //   `https://${process.env.TEST_RAIL_INSTANCE}/index.php?/api/v2/get_results_for_run/2`,
-  //   { headers }
-  // );
-  // const data = await res.json();
-  // const numberOfTestCaseResultsInRun = data.size;
-  // console.log(numberOfTestCaseResultsInRun);
-  // console.log(data.results.length);
-  // const testRailData = data.results;
-  // // console.log(testRailData);
-
-  // // return json(await res.json());
-  // return testRailData;
-
   const testRuns = await getCurrentTestRuns();
-  // console.log(testRuns);
-
-  // const testRunData = []
-  // for (let index = 0; index < testRuns.length; index++) {
-  //   const element = testRuns[index];
-  //   const metricData = testRunChartData(element);
-  //   console.log(metricData);
-  //   // testRunData.push(metricData)
-  // }
-  // const testCaseData = await getAllTestCases();
 
   const jiraDefects30Days = await getJiraBugs30Days();
   const jiraDefects30DaysProd = await getJiraBugs30DaysProd();
