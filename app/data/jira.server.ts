11732c3a44565173211d39dd934ee5c28b1602ec
Dan Reale <realed@dnb.com>
2024-01-22 12:49:39 -0500

    added defect density total stories and tce

--- app/data/jira.server.ts
+++ app/data/jira.server.ts
@@ -125,3 +125,43 @@ export async function getJiraBugs30DaysDev() {
 
   return { totalJiraIssues, jiraData };
 }
+
+export async function getJiraStories30Days() {
+  const query = `project=PLAT%26issuetype=Story%26resolved>=-30d&fields=id.key,summary&maxResults=100`;
+  const response = await axios({
+    url: `https://eyeota.atlassian.net/rest/api/2/search?jql=${query}`,
+    maxBodyLength: Infinity,
+    headers: {
+      "content-type": "application/json",
+      Accept: "application/json",
+      Authorization: `Basic ${Buffer.from(
+        process.env.JIRA_CREDENTIALS || "test"
+      ).toString("base64")}`,
+    },
+    timeout: 60000,
+    httpsAgent: new https.Agent({
+      // for self signed
+      rejectUnauthorized: false,
+      // allow legacy server
+      // need this because node 18+ removed legacy server which throws an error
+      secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+    }),
+    method: "GET",
+  });
+
+  const totalJiraIssues: number = response.data.total;
+
+  let jiraData = [];
+
+  for (let index = 0; index < response.data.issues.length; index++) {
+    const element = response.data.issues[index];
+
+    const obj: JIRA_ISSUE_DATA = {
+      key: element.key,
+      title: element.fields.summary,
+    };
+    jiraData.push(obj);
+  }
+
+  return { totalJiraIssues, jiraData };
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import { type MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import CountVisualWithLink from "~/components/ChartVisualWithLink";
+import CountVisual from "~/components/CountVisual";
 
 import Header from "~/components/Header";
 import PercentageVisual from "~/components/PercentageVisual";
@@ -11,6 +12,7 @@ import {
   getJiraBugs30Days,
   getJiraBugs30DaysDev,
   getJiraBugs30DaysProd,
+  getJiraStories30Days,
 } from "~/data/jira.server";
 import { getCurrentTestRuns, TEST_RUN_DATA } from "~/data/testrail.server";
 
@@ -27,6 +29,9 @@ export default function Index() {
     jiraDefects30Days,
     jiraDefects30DaysProd,
     jiraDefects30DaysDev,
+    testCaseEffectiveness,
+    jiraStories30Days,
+    defectDensity,
   } = useLoaderData<typeof loader>();
 
   return (
@@ -44,26 +49,25 @@ export default function Index() {
           />
         ))}
       </div>
-      <div className="grid grid-cols-4">
+      <div className="grid grid-cols-5">
         <PercentageVisual
           chartName="Test Coverage"
           percentage="12.54"
           tooltip="Test cases for Jira tickets"
         />
-        <PercentageVisual
-          chartName="Defect Density"
-          percentage="12.54"
-          tooltip="Number of defects divided by number of stories"
-        />
+        <CountVisual chartName="Defect Density" count={defectDensity} />
         <PercentageVisual
           chartName="Defect Resolution Time"
           percentage="12.54"
           tooltip="Defect closure date minus defect creation date"
         />
-        <PercentageVisual
+        <CountVisual
           chartName="Test Case Effectiveness"
-          percentage="12.54"
-          tooltip="Number of defects found divided by number of test cases executed"
+          count={parseFloat(testCaseEffectiveness)}
+        />
+        <CountVisual
+          chartName="Total Stories Completed"
+          count={jiraStories30Days.totalJiraIssues}
         />
       </div>
       <div className="grid grid-cols-3 py-20">
@@ -89,15 +93,38 @@ export default function Index() {
 
 export async function loader() {
   const testRuns = await getCurrentTestRuns();
+  const totalTestsExecuted = testRuns.reduce((accumulator, currentValue) => {
+    return (
+      accumulator +
+      currentValue.passed +
+      currentValue.failed +
+      currentValue.retest +
+      currentValue.blocked
+    );
+  }, 0);
 
   const jiraDefects30Days = await getJiraBugs30Days();
   const jiraDefects30DaysProd = await getJiraBugs30DaysProd();
   const jiraDefects30DaysDev = await getJiraBugs30DaysDev();
+  const jiraStories30Days = await getJiraStories30Days();
+
+  const testCaseEffectiveness = (
+    (jiraDefects30Days.totalJiraIssues / totalTestsExecuted) *
+    100
+  ).toFixed(2);
+
+  const defectDensity = (
+    (jiraDefects30Days.totalJiraIssues / jiraStories30Days.totalJiraIssues) *
+    100
+  ).toFixed(2);
 
   return {
     testRuns,
     jiraDefects30Days,
     jiraDefects30DaysProd,
     jiraDefects30DaysDev,
+    testCaseEffectiveness,
+    jiraStories30Days,
+    defectDensity,
   };
 }
